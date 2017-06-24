9c3c837b973adc24c153fc74e27c718dad632ea9
teknuk <dev@teknuk.com>
2017-06-24 16:33:16 +0500

    added save productform and pass data using props: product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,35 +1,49 @@
 <template>
-  <table class="table table-hover product-table text-left">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="product in productlist" track-by="id">
-        <td>{{product.name}}</td>
-        <td>{{product.description}}</td>
-        <td>{{product.price}}:-</td>
-      </tr>
-      <tr v-if="!productlist.length">
-        <td colspan="4" class="p-y-3 text-xs-center">
-          <strong>You should add some products!</strong>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <section>
+    <save-product-form :product="productInForm"></save-product-form>
+    <table class="table table-hover product-table text-left">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in productlist" track-by="id" v-on:click.prevent="onEdit(product)">
+          <td>{{product.name}}</td>
+          <td>{{product.description}}</td>
+          <td>{{product.price}}:-</td>
+        </tr>
+        <tr v-if="!productlist.length">
+          <td colspan="4" class="p-y-3 text-xs-center">
+            <strong>You should add some products!</strong>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
   import { http } from 'vue'
+  import saveProductForm from './SaveProductForm'
+
   export default {
     name: 'product',
+    components: {
+      saveProductForm
+    },
     data () {
       return {
         pageHeading: 'Product List',
-        productlist: []
+        productlist: [],
+        productInForm: {
+          id: null,
+          name: '',
+          description: '',
+          price: null
+        }
       }
     },
     created () {
@@ -37,7 +51,11 @@
       http.get('products').then(function (response) {
         _this.productlist = response.body.data
       })
+    },
+    methods: {
+      onEdit (product) {
+        this.productInForm = product
+      }
     }
   }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
