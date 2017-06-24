f309082e584ea14e1d13495b586e85af0d123eb1
teknuk <dev@teknuk.com>
2017-06-24 13:24:51 +0500

    added vue-resource library and fetched products from node product api

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,14 +1,36 @@
 <template>
-  <p> {{ message }} </p>
+  <div>
+    <h2>{{ pageHeading }}</h2>
+    <ul>
+     <li v-for="product in productlist" track-by="id">
+      {{ product.name }}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
+  import { http } from 'vue'
   export default {
     name: 'product',
     data () {
       return {
-        message: 'List Porduct Here'
+        pageHeading: 'Product List',
+        productlist: []
       }
+    },
+    created () {
+      var _this = this
+      http.get('products').then(function (response) {
+        _this.productlist = response.body.data
+      })
     }
   }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+
+// Add vue-resource as middleware to Vue.
+Vue.use(VueResource)
+
+// set the API root so we can use relative url's in our actions.
+Vue.http.options.root = 'http://localhost:3000'
 
 /* eslint-disable no-new */
 new Vue({
